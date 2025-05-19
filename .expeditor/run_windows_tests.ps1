11d52a2c0172038e575641e9c5eb1be36a32df95
John McCrae <john.mccrae@progress.com>
2025-05-19 19:12:20 +0000

    Where are GCC and Make?
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -2,6 +2,14 @@
 $ErrorActionPreference = "Stop"
 # This will run ruby test on windows platform
 
+Write-Output "--- Ensuring GCC and Make are in the path"
+$gccs = gci -path c:\opscode gcc.exe -Recurse -ErrorAction SilentlyContinue
+$env:path = "$($gccs[0].DirectoryName)" + ";" + $env:path
+
+$makes = gci -Path c:\opscode make.exe -Recurse -ErrorAction SilentlyContinue
+$env:path = "$($makes[0].DirectoryName)" + ";" + $env:path
+
+
 Write-Output "--- Bundle install"
 
 bundle config --local path vendor/bundle
