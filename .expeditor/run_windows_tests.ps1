97e79ddc319acb1c4c063cfd07335c4c94aeb009
John McCrae <john.mccrae@progress.com>
2025-05-20 14:03:49 +0000

    where the heck is gcc?
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- .expeditor/run_windows_tests.ps1
+++ .expeditor/run_windows_tests.ps1
@@ -3,10 +3,10 @@ $ErrorActionPreference = "Stop"
 # This will run ruby test on windows platform
 
 Write-Output "--- Ensuring GCC and Make are in the path"
-$gccs = gci -path c:\opscode gcc.exe -Recurse -ErrorAction SilentlyContinue
+$gccs = gci -path c:\ gcc.exe -Recurse -ErrorAction SilentlyContinue
 $env:path = "$($gccs[0].DirectoryName)" + ";" + $env:path
 
-$makes = gci -Path c:\opscode make.exe -Recurse -ErrorAction SilentlyContinue
+$makes = gci -Path c:\ make.exe -Recurse -ErrorAction SilentlyContinue
 $env:path = "$($makes[0].DirectoryName)" + ";" + $env:path
 
 
